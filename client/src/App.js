d23a0423c29bfa74349db8f6d7d3a163ac01cd86
loki <stevloki@ymail.com>
2020-03-21 09:47:26 +0700

    all page updata

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Always shows a header, even in smaller screens. */}
+        <div style={{height: '1080px', position: 'relative'}}>
+            <Layout fixedHeader>
+                <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+                    <Navigation>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                    </Navigation>
+                </Drawer>
+                <Content />
+            </Layout>
+        </div>
     </div>
   );
 }
